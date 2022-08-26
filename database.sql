6af33e44921164ae6814e732bb0ca3d954b642a9
Rifat Bin Razzaque <92661100+Rifat1028@users.noreply.github.com>
2022-08-26 14:59:37 +0600

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,29 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+CREATE TABLE `info` (
+  `id` int(11) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255)	NOT NULL,
+
+  
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `info` (`id`, `username`, `name`, `email`, `password`) VALUES
+(1, 'Testing','alex', 'email@email.com', 'Hel.152'),
+(2, 'abc','jonh', 'admin@localhost.com', '\\rn'),
+
+
+ALTER TABLE `info`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
